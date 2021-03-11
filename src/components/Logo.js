e7ce04b952763bc49bcd58ef0d34a13d9131192e
DoctorTone <drtonesoftware@gmail.com>
2021-03-11 22:34:44 +0000

    Animate text.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,21 @@
-import React from "react";
-import { useLoader } from "react-three-fiber";
+import React, { useRef, useState } from "react";
+import { useFrame } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Text from "./Text";
 
-const Logo = () => {
+const Logo = (props) => {
+  const group = useRef();
+  const [animating, setAnimating] = useState(true);
+
+  useFrame((state) => {
+    if (animating) {
+      group.current.position.y += 0.1;
+      setAnimating(group.current.position.y < 20);
+    }
+  });
+
   return (
-    <group>
+    <group ref={group}>
       <Text
         position={[0, -10, 10]}
         scale={[0.1, 0.1, 0.1]}
