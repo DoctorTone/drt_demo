3dfa13e24b6a4ea8cf0e565b7393b2184c0a3dc7
DoctorTone <drtonesoftware@gmail.com>
2021-03-11 17:33:20 +0000

    Created lots of boxes.

--- src/config/sceneAttributes.js
+++ src/config/sceneAttributes.js
@@ -1,13 +1,13 @@
 // Various attributes to control scene setup
 
 const SCENE = {
-  NUM_ROWS_X: 15,
-  NUM_ROWS_Z: 15,
-  ROW_INC_X: 5,
-  ROW_INC_Z: 5,
-  BOX_SIZE: 2,
-  X_START: -32.5,
-  Z_START: -50,
+  NUM_ROWS_X: 40,
+  NUM_ROWS_Z: 40,
+  ROW_INC_X: 3.5,
+  ROW_INC_Z: 3.5,
+  BOX_SIZE: 1.25,
+  X_START: -70,
+  Z_START: -20,
   WAVE_AMP: 2.5,
 };
 
