ad6b5013dc1796ee8727dea07d0a0a80e6ec316e
DoctorTone <drtonesoftware@gmail.com>
2021-03-11 14:32:54 +0000

    Updated row and block sizes.

--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -2,22 +2,25 @@ import React from "react";
 import * as THREE from "three";
 import { SCENE } from "../config/sceneAttributes";
 
-const NUM_ROWS_X = 5;
-const NUM_ROWS_Z = 5;
-const ROW_INC_X = 15;
-const ROW_INC_Z = 15;
-const X_START = -30;
-const Z_START = -30;
-
 const Cubes = (props) => {
   const boxGeometries = [];
   const boxPositions = [];
 
-  for (let j = 0; j < NUM_ROWS_Z; ++j) {
+  for (let j = 0; j < SCENE.NUM_ROWS_Z; ++j) {
     for (let i = 0; i < SCENE.NUM_ROWS_X; ++i) {
-      boxGeometries.push(new THREE.BoxBufferGeometry(10, 10, 10));
+      boxGeometries.push(
+        new THREE.BoxBufferGeometry(
+          SCENE.BOX_SIZE,
+          SCENE.BOX_SIZE,
+          SCENE.BOX_SIZE
+        )
+      );
       boxPositions.push(
-        new THREE.Vector3(X_START + i * ROW_INC_X, 0, Z_START + j * ROW_INC_Z)
+        new THREE.Vector3(
+          SCENE.X_START + i * SCENE.ROW_INC_X,
+          0,
+          SCENE.Z_START + j * SCENE.ROW_INC_Z
+        )
       );
     }
   }
--- src/config/sceneAttributes.js
+++ src/config/sceneAttributes.js
@@ -1,7 +1,13 @@
 // Various attributes to control scene setup
 
 const SCENE = {
-  NUM_ROWS_X: 5,
+  NUM_ROWS_X: 15,
+  NUM_ROWS_Z: 15,
+  ROW_INC_X: 5,
+  ROW_INC_Z: 5,
+  BOX_SIZE: 2.5,
+  X_START: -30,
+  Z_START: -30,
 };
 
 export { SCENE };
