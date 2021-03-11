138a2c7e194d3537318fb4ab3be47b2df15418c3
DoctorTone <drtonesoftware@gmail.com>
2021-03-11 13:53:09 +0000

    Set up box positions.

--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -3,21 +3,20 @@ import * as THREE from "three";
 
 const NUM_CUBES_X = 2;
 const NUM_CUBES_Z = 2;
+const ROW_INC = 15;
 
 const Cubes = (props) => {
   const boxGeometries = [];
-  for (
-    let box = 0, numBoxes = NUM_CUBES_X * NUM_CUBES_Z;
-    box < numBoxes;
-    ++box
-  ) {
-    boxGeometries.push(new THREE.BoxBufferGeometry(20, 10, 10));
+  const boxPositions = [];
+  for (let i = 0, numRows = NUM_CUBES_X; i < numRows; ++i) {
+    boxGeometries.push(new THREE.BoxBufferGeometry(10, 10, 10));
+    boxPositions.push(new THREE.Vector3(i * ROW_INC, 0, 0));
   }
 
   return (
     <group {...props}>
-      {boxGeometries.map((geom) => (
-        <mesh key={geom.uuid} geometry={geom} position={[0, 0, 0]}>
+      {boxGeometries.map((geom, index) => (
+        <mesh key={geom.uuid} geometry={geom} position={boxPositions[index]}>
           <meshLambertMaterial color={"yellow"} />
         </mesh>
       ))}
