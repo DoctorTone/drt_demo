cfe6c280f57a6f31692d672e70b1536c351bf36d
DoctorTone <drtonesoftware@gmail.com>
2021-03-18 22:40:05 +0000

    Alter plane size and colour.

--- src/components/Plane.js
+++ src/components/Plane.js
@@ -3,8 +3,8 @@ import React, { useRef } from "react";
 const Plane = (props) => {
   return (
     <mesh {...props} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry args={[300, 200]} />
-      <meshStandardMaterial color={"black"} />
+      <planeBufferGeometry args={[100, 100]} />
+      <meshStandardMaterial color={"blue"} />
     </mesh>
   );
 };
