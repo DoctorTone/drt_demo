68eae0d25a30b36b54060533e005c4da94432350
DoctorTone <drtonesoftware@gmail.com>
2021-03-25 14:16:09 +0000

    Added Panel component to place text.

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Panel = ({ displayText }) => {
-  return <p>{displayText}</p>;
+const Panel = ({ displayText, x, y }) => {
+  const panelStyle = {
+    left: `${x}%`,
+    top: `${y}%`,
+  };
+  return (
+    <p className="position-absolute" style={panelStyle}>
+      {displayText}
+    </p>
+  );
 };
 
 export default Panel;
