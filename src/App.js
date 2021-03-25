3be82da200029d829bc3513a577357d44c8a5af0
DoctorTone <drtonesoftware@gmail.com>
2021-03-25 22:46:07 +0000

    Add float nav bar links.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Experiments from "./pages/Experiments";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/experiments" component={Experiments} />
+        <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={About} />
       </Switch>
     </Router>
