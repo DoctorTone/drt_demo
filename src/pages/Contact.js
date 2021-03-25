0ce99b79cf448b5c2eeec90a25dd59e1b287c432
DoctorTone <drtonesoftware@gmail.com>
2021-03-25 22:50:09 +0000

    Navbars on other pages.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Contact = () => {
   return (
     <div>
+      <Navbar />
       <p>Please contact us</p>
     </div>
   );
--- src/pages/Experiments.js
+++ src/pages/Experiments.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Experiments = () => {
   return (
     <div>
+      <Navbar />
       <p>Experiments</p>
     </div>
   );
