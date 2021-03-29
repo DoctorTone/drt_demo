4b9dde0ce28a191edbed8d1f85f3f21cef1792fe
DoctorTone <drtonesoftware@gmail.com>
2021-03-29 23:45:41 +0100

    Add animation effects to images.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Bounce from "react-reveal/Bounce";
+import Flip from "react-reveal/Flip";
+import Zoom from "react-reveal/Zoom";
 
 const Portfolio = () => {
   return (
@@ -11,20 +14,26 @@ const Portfolio = () => {
       <div className="container">
         <div className="row">
           <div className="col-4 img-overlay">
-            <img
-              className="img-fluid rounded-lg"
-              src="volumeRender.jpg"
-              alt=""
-            />
+            <Zoom>
+              <img
+                className="img-fluid rounded-lg"
+                src="volumeRender.jpg"
+                alt=""
+              />
+            </Zoom>
             <div className="img-text-overlay">
               <h4>Medical Visualisation</h4>
             </div>
           </div>
           <div className="col-4">
-            <img className="img-fluid rounded" src="FTSEViz.jpg" alt="" />
+            <Zoom>
+              <img className="img-fluid rounded" src="FTSEViz.jpg" alt="" />
+            </Zoom>
           </div>
           <div className="col-4">
-            <img className="img-fluid rounded" src="pandemic.jpg" alt="" />
+            <Zoom>
+              <img className="img-fluid rounded" src="pandemic.jpg" alt="" />
+            </Zoom>
           </div>
         </div>
       </div>
