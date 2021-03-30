1701ba034d0261615ccf705a91abe9d2acf9fdff
DoctorTone <drtonesoftware@gmail.com>
2021-03-30 15:10:04 +0100

    Neon image overlay.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -39,8 +39,11 @@ const Portfolio = () => {
               <h4>3D Data Visualisation</h4>
             </div>
           </div>
-          <div className="col-4 text-center">
+          <div className="col-4 text-center img-overlay">
             <img className="img-fluid rounded" src="NEON Logo.png" alt="" />
+            <div className="img-text-overlay">
+              <h4>Medical Trials</h4>
+            </div>
           </div>
         </div>
       </div>
