1ac0e65fd8b9cd75b6ea8865a65c65a844c1e967
DoctorTone <drtonesoftware@gmail.com>
2021-04-01 23:46:03 +0100

    Page transition with transition group.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React, { useContext } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -9,18 +10,24 @@ import Contact from "./pages/Contact";
 import MedicalViz from "./pages/MedicalViz";
 
 function App() {
-  const location = useLocation();
-
   return (
     <>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/portfolio/medical" component={MedicalViz} />
-        <Route exact path="/experiments" component={Experiments} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/about" component={About} />
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={750} classNames="fade">
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/portfolio" component={Portfolio} />
+                <Route exact path="/portfolio/medical" component={MedicalViz} />
+                <Route exact path="/experiments" component={Experiments} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </>
   );
 }
