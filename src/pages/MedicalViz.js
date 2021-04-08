5e6642fba4c69dcb4ca3ee6e8de215c98e92e72c
DoctorTone <drtonesoftware@gmail.com>
2021-04-08 15:00:02 +0100

    Add buttons for demo and video.

--- src/pages/MedicalViz.js
+++ src/pages/MedicalViz.js
@@ -12,18 +12,37 @@ const MedicalViz = () => {
       <hr className="h-line mb-5" />
       <div className="container">
         <div className="row">
-          <div className="col-4 offset-4">
+          <div className="col-4 offset-4 mb-3">
             <img
               className="img-fluid rounded-lg"
               src="../volumeRender.jpg"
               alt=""
             />
           </div>
-          <p>
-            This application takes a number of medical images and creates a
-            volume that can then be manipulated more easily (often referred to
-            as volume rendering).
-          </p>
+          <div>
+            <p className="display-6 text-center">Description</p>
+            <p>
+              This application takes a number of medical images and creates a
+              volume that can then be manipulated more easily (often referred to
+              as volume rendering).
+            </p>
+          </div>
+          <div className="mx-auto">
+            <a
+              href="https://www.drt-software.com/MedicalViz/medicalViz.html"
+              className="btn btn-primary mr-3"
+              target="_blank"
+            >
+              Launch demo
+            </a>
+            <a
+              href="https://www.drt-software.com/MedicalViz/medicalViz.html"
+              className="btn btn-secondary"
+              target="_blank"
+            >
+              Watch video
+            </a>
+          </div>
         </div>
       </div>
     </div>
