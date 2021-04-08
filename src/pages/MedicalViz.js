a83c65a30f9761c646ac01da1b240c50a06fb0a9
DoctorTone <drtonesoftware@gmail.com>
2021-04-08 16:09:24 +0100

    Adjusted layout for medical page.

--- src/pages/MedicalViz.js
+++ src/pages/MedicalViz.js
@@ -7,49 +7,51 @@ const MedicalViz = () => {
   return (
     <div>
       <Navbar activeLink="Portfolio" />
-      <hr className="mt-5 h-line" />
-      <h2 className="indent orangeText display-4">
-        Portfolio - <span className="display-6">Medical Visualisation</span>
-      </h2>
-      <hr className="h-line mb-5" />
       <div className="container">
-        <div className="row">
-          <div className="col-4 offset-4 mb-3">
-            <img
-              className="img-fluid rounded-lg"
-              src="../volumeRender.jpg"
-              alt=""
-            />
+        <hr className="mt-5 h-line" />
+        <h2 className="orangeText display-4">
+          Portfolio - <span className="display-6">Medical Visualisation</span>
+        </h2>
+        <hr className="h-line mb-5" />
+        <div className="container">
+          <div className="row">
+            <div className="col-4 offset-4 mb-3">
+              <img
+                className="img-fluid rounded-lg"
+                src="../volumeRender.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <p className="display-6 text-center">Description</p>
+              <p>
+                This application takes a number of medical images and creates a
+                volume that can then be manipulated more easily (often referred
+                to as volume rendering).
+              </p>
+            </div>
+            <div className="mx-auto">
+              <a
+                href="https://www.drt-software.com/MedicalViz/medicalViz.html"
+                className="btn btn-primary mr-3"
+                target="_blank"
+              >
+                Launch demo
+              </a>
+              <a
+                href="https://www.youtube.com/watch?v=MoKVrdJoqAM"
+                className="btn btn-warning"
+                target="_blank"
+              >
+                Watch video
+              </a>
+            </div>
           </div>
-          <div>
-            <p className="display-6 text-center">Description</p>
-            <p>
-              This application takes a number of medical images and creates a
-              volume that can then be manipulated more easily (often referred to
-              as volume rendering).
-            </p>
-          </div>
-          <div className="mx-auto">
-            <a
-              href="https://www.drt-software.com/MedicalViz/medicalViz.html"
-              className="btn btn-primary mr-3"
-              target="_blank"
-            >
-              Launch demo
-            </a>
-            <a
-              href="https://www.youtube.com/watch?v=MoKVrdJoqAM"
-              className="btn btn-warning"
-              target="_blank"
-            >
-              Watch video
-            </a>
-          </div>
+          <hr className="mt-5 h-line" />
+          <Info />
+          <hr className="mt-5 h-line" />
+          <Footer />
         </div>
-        <hr className="mt-5 h-line" />
-        <Info />
-        <hr className="mt-5 h-line" />
-        <Footer />
       </div>
     </div>
   );
