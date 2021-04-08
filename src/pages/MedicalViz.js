06242c087ece39990d756c367b2b938d56785b88
DoctorTone <drtonesoftware@gmail.com>
2021-04-08 14:35:06 +0100

    Added more display headings.

--- src/pages/MedicalViz.js
+++ src/pages/MedicalViz.js
@@ -7,7 +7,7 @@ const MedicalViz = () => {
       <Navbar activeLink="Portfolio" />
       <hr className="mt-5 h-line" />
       <h2 className="indent orangeText display-4">
-        Portfolio - <span className="h1">Medical Viz</span>
+        Portfolio - <span className="display-6">Medical Viz</span>
       </h2>
       <hr className="h-line mb-5" />
       <div className="container">
@@ -19,6 +19,11 @@ const MedicalViz = () => {
               alt=""
             />
           </div>
+          <p>
+            This application takes a number of medical images and creates a
+            volume that can then be manipulated more easily (often referred to
+            as volume rendering).
+          </p>
         </div>
       </div>
     </div>
