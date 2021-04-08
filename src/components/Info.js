643f6c543635b527050c6814bae25d5f77cc911e
DoctorTone <drtonesoftware@gmail.com>
2021-04-08 11:30:29 +0100

    Add font awesome.

--- src/components/Info.js
+++ src/components/Info.js
@@ -17,10 +17,12 @@ const Info = () => {
           </p>
         </div>
         <div className="col-4">
-          <p>Contact</p>
+          <p className="orangeText">Contact</p>
+          <p>enquiries@drt-software.com</p>
+          <p>0115 8440840</p>
         </div>
         <div className="col-4">
-          <p>Social</p>
+          <p className="orangeText">Social</p>
         </div>
       </div>
     </div>
