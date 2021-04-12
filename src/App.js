20f5af543904738bb5df04b260f865a06808983a
DoctorTone <drtonesoftware@gmail.com>
2021-04-12 23:05:34 +0100

    Remove canvas wrapper.

--- src/App.js
+++ src/App.js
@@ -7,32 +7,22 @@ import Logo from "./components/Logo";
 import Box from "./components/Box";
 
 function App() {
-  const renderStyle = {
-    position: "absolute",
-    top: "0",
-    left: "0",
-    right: "0",
-    bottom: "0",
-  };
-
   return (
-    <div style={renderStyle}>
-      <Canvas className="w-100 h-100" camera={{ position: [11, 32, 35] }}>
-        <CameraControls />
-        <spotLight
-          position={[15, 50, 7]}
-          angle={0.45}
-          intensity={2.0}
-          penumbra={0.49}
-          decay={1}
-        />
-        <Cubes color={"grey"} />
-        <Box position={[50, 50, 7]} />
-        <Suspense fallback={null}>
-          <Logo />
-        </Suspense>
-      </Canvas>
-    </div>
+    <Canvas className="w-100 h-100" camera={{ position: [11, 32, 35] }}>
+      <CameraControls />
+      <spotLight
+        position={[15, 50, 7]}
+        angle={0.45}
+        intensity={2.0}
+        penumbra={0.49}
+        decay={1}
+      />
+      <Cubes color={"grey"} />
+      <Box position={[50, 50, 7]} />
+      <Suspense fallback={null}>
+        <Logo />
+      </Suspense>
+    </Canvas>
   );
 }
 
