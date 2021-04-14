7d1d67ec74d2e39c9954b987798d4b77ecc31e0a
DoctorTone <drtonesoftware@gmail.com>
2021-04-14 15:43:26 +0100

    Styled slider.

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Slider from "@material-ui/core/Slider";
+import { withStyles } from "@material-ui/core/styles";
 
 const ControlPanel = () => {
   const panelStyle = {
     position: "absolute",
-    top: "1%",
+    top: "5%",
     left: "5%",
-    width: "50%",
+    width: "25%",
     zIndex: "1",
   };
 
+  const PosSlider = withStyles({
+    root: {
+      color: "white",
+    },
+  })(Slider);
+
   return (
     <div style={panelStyle}>
-      <Slider />
+      <PosSlider defaultValue={0} min={0} max={100} />
     </div>
   );
 };
